d21984c537a11df569555622c0f82677a6990496
Chamon <luizchamon@gmail.com>
2015-06-15 20:09:34 -0300

    Adjust pin registers.
    Signed-off-by: Chamon <luizchamon@gmail.com>

--- adi131e08.h
+++ adi131e08.h
@@ -4,13 +4,13 @@
 /***************************************************************/
 
 /* Pin definitions */
-#define SPI_SCLK
-#define SPI_MOSI
-#define SPI_MISO
-#define SPI_CS
-#define START
-#define RESET
-#define DRDY
+#define SPI_SCLK r30.t5 /* PRU1_5 GPIO2_11 P8_42 */
+#define SPI_MOSI r30.t0 /* PRU1_0 GPIO2_6  P8_45 */ /* DIN */
+#define SPI_MISO r31.t3 /* PRU1_3 GPIO2_9  P8_44 */ /* DOUT */
+#define SPI_CS   r30.t7 /* PRU1_7 GPIO2_13 P8_40 */
+#define START    r30.t4 /* PRU1_4 GPIO2_10 P8_41 */
+#define RESET    r30.t2 /* PRU1_2 GPIO2_8  P8_43 */
+#define DRDY     r31.t1 /* PRU1_1 GPIO2_7  P8_46 */
 
 /* 50 ns < t_SCLK < (t_DR – 4 t_CLK) / (N_bits*N_channels + 24) */
 /* Using main clock @ 2.048 MHz:
