9e1dbcea28628eefa24a0ffc22c203aec78a0a0b
Chamon <[luizchamon] [gmail]>
2015-06-18 16:49:36 -0300

    * Use r29 instead of r0; * Fix indentations; * Include warning about r29.
    Signed-off-by: Chamon <[luizchamon] [gmail]>

--- spi.h
+++ spi.h
@@ -2,6 +2,11 @@
 /* SPI bit-banging macros */
 /**************************/
 
+/* NOTE: Before including this file, define SPI_SCLK, SPI_MOSI, SPI_MISO,
+   SPI_CS, and SPI_SCLK_DELAY */
+
+/* WARNING: Most of these macros modify r29 without warning. */
+
 #ifndef SPI_SCLK
   #error SPI SCLK pin undefined.
 #endif
@@ -24,27 +29,27 @@
 
 
 .macro NOP
-    MOV r0, r0
+    MOV     r0, r0
 .endm
 
 
 .macro SPI_WAIT
 .mparam delay, stop_early = 0
-    MOV r0, delay
-    SUB r0, r0, 2
-    SUB r0, r0, stop_early
+    MOV     r29, delay
+    SUB     r29, r29, 2
+    SUB     r29, r29, stop_early
     NOP
     DELAY:
-        SUB r0, r0, 1
-        QBNE DELAY, r0, 0
+        SUB     r29, r29, 1
+        QBNE    DELAY, r29, 0
 .endm
 
 
 .macro LBIT
 .mparam bit1, bit2
-    QBBS HIGH, bit1
-    CLR bit2
-    QBA END
+    QBBS    HIGH, bit1
+    CLR     bit2
+    QBA     END
     HIGH:
         SET bit2
         NOP
@@ -54,10 +59,10 @@
 
 .macro SPI_TX_CLK
 .mparam delay
-    SET SPI_SCLK              // SCLK = 1
-    SPI_WAIT SPI_SCLK_DELAY
-    CLR SPI_SCLK              // SCLK = 0
-    SPI_WAIT SPI_SCLK_DELAY, 1
+    SET         SPI_SCLK              // SCLK = 1
+    SPI_WAIT    SPI_SCLK_DELAY
+    CLR         SPI_SCLK              // SCLK = 0
+    SPI_WAIT    SPI_SCLK_DELAY, 1
     NOP
     // Room for one more instruction before SCLK = 1
 .endm
@@ -65,22 +70,22 @@
 
 .macro SPI_TX
 .mparam MSG_REG
-    LBIT MSG_REG.t7, SPI_MOSI   // Set output (bit 7)
-    SPI_TX_CLK SPI_CLOCK
-    LBIT MSG_REG.t6, SPI_MOSI   // Set output (bit 6)
-    SPI_TX_CLK SPI_CLOCK
-    LBIT MSG_REG.t5, SPI_MOSI   // Set output (bit 5)
-    SPI_TX_CLK SPI_CLOCK
-    LBIT MSG_REG.t4, SPI_MOSI   // Set output (bit 4)
-    SPI_TX_CLK SPI_CLOCK
-    LBIT MSG_REG.t3, SPI_MOSI   // Set output (bit 3)
-    SPI_TX_CLK SPI_CLOCK
-    LBIT MSG_REG.t2, SPI_MOSI   // Set output (bit 2)
-    SPI_TX_CLK SPI_CLOCK
-    LBIT MSG_REG.t1, SPI_MOSI   // Set output (bit 1)
-    SPI_TX_CLK SPI_CLOCK
-    LBIT MSG_REG.t0, SPI_MOSI   // Set output (bit 0)
-    SPI_TX_CLK SPI_CLOCK
+    LBIT        MSG_REG.t7, SPI_MOSI   // Set output (bit 7)
+    SPI_TX_CLK  SPI_CLOCK
+    LBIT        MSG_REG.t6, SPI_MOSI   // Set output (bit 6)
+    SPI_TX_CLK  SPI_CLOCK
+    LBIT        MSG_REG.t5, SPI_MOSI   // Set output (bit 5)
+    SPI_TX_CLK  SPI_CLOCK
+    LBIT        MSG_REG.t4, SPI_MOSI   // Set output (bit 4)
+    SPI_TX_CLK  SPI_CLOCK
+    LBIT        MSG_REG.t3, SPI_MOSI   // Set output (bit 3)
+    SPI_TX_CLK  SPI_CLOCK
+    LBIT        MSG_REG.t2, SPI_MOSI   // Set output (bit 2)
+    SPI_TX_CLK  SPI_CLOCK
+    LBIT        MSG_REG.t1, SPI_MOSI   // Set output (bit 1)
+    SPI_TX_CLK  SPI_CLOCK
+    LBIT        MSG_REG.t0, SPI_MOSI   // Set output (bit 0)
+    SPI_TX_CLK  SPI_CLOCK
     NOP
 
     CLR SPI_MOSI                // Reset output to low
@@ -89,10 +94,10 @@
 
 .macro SPI_RX_CLK
 .mparam delay
-    CLR SPI_SCLK              // SCLK = 0
-    SPI_WAIT SPI_SCLK_DELAY
-    SET SPI_SCLK              // SCLK = 1
-    SPI_WAIT SPI_SCLK_DELAY, 1
+    CLR         SPI_SCLK              // SCLK = 0
+    SPI_WAIT    SPI_SCLK_DELAY
+    SET         SPI_SCLK              // SCLK = 1
+    SPI_WAIT    SPI_SCLK_DELAY, 1
     NOP
     // Room for one more instruction before SCLK = 1
 .endm
@@ -100,26 +105,26 @@
 
 .macro SPI_RX
 .mparam MSG_REG
-    SET SPI_SCLK                // SCLK = 1
-    SPI_WAIT SPI_SCLK_DELAY, 1
+    SET         SPI_SCLK                // SCLK = 1
+    SPI_WAIT    SPI_SCLK_DELAY, 1
     NOP
 
-    LBIT SPI_MISO, MSG_REG.t7   // Get input (bit 7)
-    SPI_RX_CLK SPI_CLOCK
-    LBIT SPI_MISO, MSG_REG.t6   // Get input (bit 6)
-    SPI_RX_CLK SPI_CLOCK
-    LBIT SPI_MISO, MSG_REG.t5   // Get input (bit 5)
-    SPI_RX_CLK SPI_CLOCK
-    LBIT SPI_MISO, MSG_REG.t4   // Get input (bit 4)
-    SPI_RX_CLK SPI_CLOCK
-    LBIT SPI_MISO, MSG_REG.t3   // Get input (bit 3)
-    SPI_RX_CLK SPI_CLOCK
-    LBIT SPI_MISO, MSG_REG.t2   // Get input (bit 2)
-    SPI_RX_CLK SPI_CLOCK
-    LBIT SPI_MISO, MSG_REG.t1   // Get input (bit 1)
-    SPI_RX_CLK SPI_CLOCK
-    LBIT SPI_MISO, MSG_REG.t0   // Get input (bit 0)
+    LBIT        SPI_MISO, MSG_REG.t7    // Get input (bit 7)
+    SPI_RX_CLK  SPI_CLOCK
+    LBIT        SPI_MISO, MSG_REG.t6    // Get input (bit 6)
+    SPI_RX_CLK  SPI_CLOCK
+    LBIT        SPI_MISO, MSG_REG.t5    // Get input (bit 5)
+    SPI_RX_CLK  SPI_CLOCK
+    LBIT        SPI_MISO, MSG_REG.t4    // Get input (bit 4)
+    SPI_RX_CLK  SPI_CLOCK
+    LBIT        SPI_MISO, MSG_REG.t3    // Get input (bit 3)
+    SPI_RX_CLK  SPI_CLOCK
+    LBIT        SPI_MISO, MSG_REG.t2    // Get input (bit 2)
+    SPI_RX_CLK  SPI_CLOCK
+    LBIT        SPI_MISO, MSG_REG.t1    // Get input (bit 1)
+    SPI_RX_CLK  SPI_CLOCK
+    LBIT        SPI_MISO, MSG_REG.t0    // Get input (bit 0)
 
-    CLR SPI_SCLK                // SCLK = 0
-    SPI_WAIT SPI_SCLK_DELAY
+    CLR         SPI_SCLK                // SCLK = 0
+    SPI_WAIT    SPI_SCLK_DELAY
 .endm
