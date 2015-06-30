88fac8c503415a738981c1abb50c19d9f509a7b4
Chamon <luizchamon@gmail.com>
2015-06-30 16:17:52 -0300

    Port SPI library test to TI's PRU compiler.
    Signed-off-by: Chamon <luizchamon@gmail.com>

---
+++ tests/spi/bin_pru.cmd
@@ -0,0 +1,10 @@
+-b
+-image
+
+ROMS
+{
+    PAGE 0:
+        text: origin = 0x0, length = 0x2000, files={spi_test.bin}
+    PAGE 1:
+        data: origin = 0x0, length = 0x2000, files={spi_test.data}
+}
--- tests/spi/spi_test.asm
+++ tests/spi/spi_test.asm
@@ -1,49 +1,52 @@
-.origin 0                        // start of program in PRU memory
-.entrypoint BEGIN                // program entry point
+INS_PER_US          .set    200
+INS_PER_DELAY_LOOP  .set    2
+DELAY_US            .set    100 * 1000 * (INS_PER_US / INS_PER_DELAY_LOOP)
 
-#define INS_PER_US 200
-#define INS_PER_DELAY_LOOP 2
-#define DELAY_US  100 * 1000 * (INS_PER_US / INS_PER_DELAY_LOOP)
+SPI_SCLK            .set    r30.t5      ; P8_42
+SPI_MOSI            .set    r30.t0      ; P8_45
+SPI_MISO            .set    r31.t3      ; P8_44
+SPI_CS              .set    r30.t7      ; P8_40
 
-#define SPI_SCLK r30.t5 /* P8_42 */
-#define SPI_MOSI r30.t0 /* P8_45 */
-#define SPI_MISO r31.t3 /* P8_44 */
-#define SPI_CS   r30.t7 /* P8_40 */
+SPI_SCLK_DELAY      .set    9
 
-#define SPI_SCLK_DELAY 9
-
-#define PRU0_R31_VEC_VALID 32
-#define PRU_EVTOUT_1 4
-
-#include "../../spi.h"
+PRU_INT_VALID       .set    32
+PRU_EVTOUT_1        .set    4
 
 
-BEGIN:
-// Enable the OCP master port
-LBCO    r0, C4, 4, 4    // load SYSCFG reg into r0 (use c4 const addr)
-CLR     r0, r0, 4       // clear bit 4 (STANDBY_INIT)
-SBCO    r0, C4, 4, 4    // store the modified r0 back at the load addr
+.include "../../spi.h"
 
 
-// Start test
-MOV     r1, 0xF1AA
-SPI_TX  r1.b0   // Send 0b10101010
-SPI_RX  r1.b2   // Receive data on third byte of r1
-SPI_TX  r1.b1   // Send 0b11110001
+; Code starts here
+    .text
+    .retain
+    .retainrefs
+    .global         main
 
-// Store received data in RAM
-MOV     r3, 0           // Load RAM address to r3
-SBBO    r1, r3, 0, 4    // Store data
-
-
-// Wait 100 ms
-MOV     r2, DELAY_US
-DELAY:
-SUB     r2, r2, 1
-QBNE    DELAY, r2, 0
+main:
+; Enable the OCP master port
+    LBCO    r0, C4, 4, 4    ; load SYSCFG reg into r0 (use c4 const addr)
+    CLR     r0, r0, 4       ; clear bit 4 (STANDBY_INIT)
+    SBCO    r0, C4, 4, 4    ; store the modified r0 back at the load addr
 
 
-END:
-// Send interrupt to host
-MOV     R31.b0, PRU0_R31_VEC_VALID | PRU_EVTOUT_1
-HALT
+; Start test
+    LDI32   r1, 0xF1AA
+    SPI_TX  r1.b0           ; Send 0b10101010
+    SPI_RX  r1.b2           ; Receive data on third byte of r1
+    SPI_TX  r1.b1           ; Send 0b11110001
+
+; Store received data in RAM
+    MOV     r3, 0           ; Load RAM address to r3
+    SBBO    r1, r3, 0, 4    ; Store data
+
+
+; Wait 100 ms
+    LDI32   r2, DELAY_US
+delay:
+    SUB     r2, r2, 1
+    QBNE    DELAY, r2, 0
+
+
+; Send interrupt to host and stop PRU
+    LDI     r31.b0, PRU_INT_VALID + PRU_EVTOUT_1
+    HALT
